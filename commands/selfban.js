b05623b175b2793e0c2088fd943a6bef62a05db3
Ragav Srinivasan <56784771+Rageon787@users.noreply.github.com>
2025-02-17 10:31:07 +1100

    Feat: banned role assignment and deletion

--- commands/selfban.js
+++ commands/selfban.js
@@ -3,7 +3,8 @@ const {
   PermissionFlagsBits,
   SlashCommandBuilder,
 } = require("discord.js");
-
+const { roleId } = require("../config.json");
+const interactionCreate = require("../events/interactionCreate");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("selfban")
@@ -39,11 +40,33 @@ module.exports = {
         ),
     ),
   async execute(interaction) {
-    const subcommand = interaction.options.getSubcommand;
-    if (subcommand == "timeblock") {
-    } else if (subcommand == "staggered") {
+    const subcommand = interaction.options.getSubcommand();
+    const durationArgs = interaction.options.getString();
+    console.log(subcommand);
+    if (subcommand === "timeblock") {
+      // Perform input validation? is it the right format?
+      // parse the time string ("y mo d h m") -to ms
+      // Ban this user immediately and store the unban time in a database
+      await interaction.member.roles.add(roleId);
+      setTimeout(() => interaction.member.roles.remove(roleId), 5000);
+      await interaction.reply(
+        `${interaction.user} you're banned for ${durationArgs}`,
+      );
+    } else if (subcommand === "staggered") {
+      // two string options: ban time and unban time?
+      // perform input validation? is it in the expected format?
+      // if no then send a message saying "Invalid time format"
+      // else
+      // parse everything into ms
+      // Ban this user immediately and store the unban time in a database called Unbans
+      // The next ban time will also be stored in the database called Bans
+      await interaction.member.roles.add(roleId);
+      setTimeout(() => interaction.member.roles.remove(roleId), 5000);
+      await interaction.reply(
+        `${interaction.user} you're banned for ${durationArgs}`,
+      );
+    } else {
+      await interaction.reply("something is wrong?");
     }
-
-    await interaction.reply("you're banned now");
   },
 };
